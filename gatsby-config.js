b23e185c0dde3d54e0f7f39b98fc1d4de4539fd6
mauthi <michael.mauthner@netwerker.at>
2022-11-16 15:52:14 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `netWERKER Gatsby Test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
